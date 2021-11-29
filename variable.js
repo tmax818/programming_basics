9bd3d55667a2cfad666e586ac936247906c27abc
Edward <eddieim@gmail.com>
2021-11-29 13:53:30 -0800

    W1D1

---
+++ variable.js
@@ -0,0 +1,55 @@
+// DECLARING AND INITIALIZING A VARIABLE
+var my_age = 31
+
+// DECLARING
+var another_age
+// INITIAZLING
+another_age = 33
+
+
+// ADDITION
+my_age = my_age + 7
+my_age += 4
+
+my_age++
+console.log(my_age)
+// SUBTRACTION
+my_age = my_age - 2
+my_age -= 4
+
+my_age--
+console.log(my_age)
+// MULTIPLICATION
+my_age = my_age * 2
+my_age *= 2
+console.log(my_age)
+// DIVISION
+my_age = my_age / 4
+my_age /= 6
+
+console.log(my_age)
+
+
+// STRINGS
+var first_name = "Edward"
+var last_name = "Im"
+
+// CONCATENATION OR CONCAT FOR SHORT
+console.log(first_name + " " + last_name)
+
+
+var num1 = 123
+var num2 = "456"
+
+console.log(num1 + num2)
+
+// BOOLEAN
+var check = true
+var another_check = false
+
+
+// UNDEFINED VS NULL
+var something
+something = null
+
+console.log(something)
