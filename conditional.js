fe261a95451b3b5dbc2814986181531203b2b196
Edward <eddieim@gmail.com>
2021-11-30 09:21:29 -0800

    W1D2

---
+++ conditional.js
@@ -0,0 +1,25 @@
+// FACEBOOK LOGIN LOGIC
+
+var prev_logged = false
+
+if(prev_logged){
+    console.log("Here is your feed")
+}
+else{
+    console.log("Here is the LOGIN PAGE")
+}
+
+
+
+// AGE CHECKER
+var age = 21
+
+if(age >= 21){
+    console.log("You can buy some alcohol and tobacco")
+}
+else if(age >= 18){
+    console.log("You can vote and buy lottery tickets")
+}
+else{
+    console.log("You can go get your permit and driver license")
+}
