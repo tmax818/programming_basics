40ea93537878c0d08578c610e846f03de8ad92c8
Edward Im <eddieim@gmail.com>
2022-02-07 13:38:42 -0800

    W1D1

---
+++ conditional.js
@@ -0,0 +1,4 @@
+var height = 35
+
+
+// THIS IS A COMMENT
---
+++ variable.js
@@ -0,0 +1,64 @@
+// DECLARING AND INITIALIZING A VARIABLE
+var number = 7
+
+// DECLARING VARIABLE 
+var myAge
+// INITIALIZING A VALUE
+myAge = 31
+
+
+
+// ADDITION
+myAge = myAge + 3
+myAge += 3
+
+myAge++ // adding by 1
+var myBrother = 34
+
+
+myAge = myBrother + 1
+// SUBTRACTION
+myAge = myAge - 1
+myAge -= 1
+
+myAge--
+// MULTIPLICATION
+myAge = myAge * 2
+myAge *= 2
+
+console.log(age)
+// DIVISION
+myAge = myAge / 2
+myAge /= 3
+
+// MODULOUS
+console.log(myAge % 3)
+
+
+
+// STRINGS
+var firstName = "Edward"
+var lastName = 'Im'
+// CONCATENATION OR CONCAT FOR SHORT
+var fullName = firstName + " 7" + lastName
+console.log("Hello my name is " + firstName + " " + lastName)
+
+// STRING INTERPOLATION
+console.log(`My name is ${firstName} ${lastName}`)
+
+var num1 = 123
+var num2 = "456"
+
+console.log(num1 + num2)
+
+// BOOLEAN
+var check = true
+var anotherCheck = false
+
+
+// UNDEFINED VS NULL
+var something
+
+something += 1
+
+console.log(something)
