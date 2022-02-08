8503f35288de8242553b74018cd41816c67240ff
Edward Im <eddieim@gmail.com>
2022-02-08 13:40:02 -0800

    W1D2 afternoon

---
+++ t-diagram.js
@@ -0,0 +1,79 @@
+// T-Diagrams
+
+var num1 = 20;
+var num2 = 5;
+if (num1 < num2) {
+    num2 = num2 * num1;
+}
+else {
+    num1 = num1 / num2;
+}
+if (num1 < num2) {
+    num1 = num1 * 2;
+}
+else if (num1 == num2) {
+    num2 = num1 * num2;
+}
+else {
+    num2 = num2 * 2;
+}
+console.log(num1);
+console.log(num2);
+
+
+// ---------------------------------------------------------------- //
+x = 15
+y = 30
+z = 0
+
+if (x >= 20 && y <= 15) {
+    z = x * y
+}
+else if (x == 15 && y < 20) {
+    z = x + y
+}
+else if (x == 15 && y == 20) {
+    z = x * y
+}
+else {
+    z = z + x / y
+}
+console.log(z)
+
+// ---------------------------------------------------------------- //
+
+var isSunny = true;
+var temperature = 45; // let's assume degrees Fahrenheit
+var isRaining = false;
+var whatToBring = "I should bring: ";
+
+if (isSunny) {
+    whatToBring += "sunglasses, ";
+}
+if (temperature < 50) {
+    whatToBring += "a coat, ";
+}
+if (isRaining) {
+    whatToBring += "an umbrella, ";
+}
+whatToBring += "and a smile!";
+
+console.log(whatToBring);
+
+// ---------------------------------------------------------------- //
+var num1 = 20;
+var num2 = 5;
+if (num1 < num2) {
+    num2 = num2 * num1;
+} else {
+    num1 = num1 / num2;
+    if (num1 < num2){
+        num1 = num1 * 2;
+    } else if (num1 == num2){
+        num2 = num1 * num2;
+    } else {
+        num2 = num2 * 2;
+    }
+}
+console.log(num1);
+console.log(num2);
